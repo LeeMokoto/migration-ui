3db05ea91c57bdfa9dd974d8d32cb34029a30d2d
Leepo.Mokoto <leepo.mokoto@disraptor.co.za>
2024-02-26 15:18:03 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
 
-function App() {
+import { useEffect, useState } from "react"
+import { Uploader } from "./uploader"
+
+import CircularProgress from '@mui/material/CircularProgress';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+
+export default function App() {
+  const [file, setFile] = useState(undefined)
+  const [uploader, setUploader] = useState(undefined)
+  const [percentage, setPercentage] = useState(0)
+
+
+  useEffect(() => {
+    if (file) {
+      let percentage = undefined
+
+     const videoUploaderOptions = {
+        fileName: "foo",
+        file: file,
+      }
+      const uploader = new Uploader(videoUploaderOptions)
+      setUploader(uploader)
+
+      uploader
+        .onProgress(({ percentage: newPercentage }) => {
+          // to avoid the same percentage to be logged twice
+          if (newPercentage !== percentage) {
+            percentage = newPercentage
+            setPercentage(percentage)
+          }
+        })
+        .onError((error) => {
+          setFile(undefined)
+          console.error(error)
+        })
+
+      uploader.start()
+    }
+  }, [file])
+
+  const onCancel = () => {
+    if (uploader) {
+      uploader.abort()
+      setFile(undefined)
+    }
+  }
+
+  function CircularProgressWithLabel(props) {
+    return (
+      <Box sx={{ position: 'relative', display: 'inline-flex' }}>
+        <CircularProgress variant="determinate" {...props} />
+        <Box
+          sx={{
+            top: 0,
+            left: 0,
+            bottom: 0,
+            right: 0,
+            position: 'absolute',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <Typography variant="caption" component="div" color="text.secondary">
+            {`${Math.round(props.value)}%`}
+          </Typography>
+        </Box>
+      </Box>
+    );
+  }
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Upload your file</h1>
+      <div>
+        <input
+          type="file"
+          onChange={(e) => {
+            setFile(e.target?.files?.[0])
+          }}
+        />
+      </div>
+      <Box sx={{marginBottom: "20px"}}>
+        <button onClick={onCancel}>Cancel</button>
+      </Box>
+      <CircularProgressWithLabel value={percentage} />
     </div>
-  );
+  )
 }
-
-export default App;
